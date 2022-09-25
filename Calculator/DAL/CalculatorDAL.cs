2e9a93f4d2c64b766b1512cf0f88d759582b1809
dell <dell@DESKTOP-GLC7ANC>
2022-09-25 00:11:57 -0400

    Add project files.

---
+++ Calculator/DAL/CalculatorDAL.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator.DAL
+{
+    public class CalculatorDAL
+    {
+        public static int Sum(int val1, int val2)
+        {
+            int result = val1 + val2;
+            return result;
+        }
+
+        public static int Subtraction(int val1, int val2)
+        {
+            int result = val1 - val2;
+            return result;
+        }
+
+        public static int Multiplication(int val1, int val2)
+        {
+            int result = val1 * val2;
+            return result;
+        }
+
+        public static int Division(int val1, int val2)
+        {
+            int result = val1 / val2;
+            return result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using Calculator.DAL;
+using System;
+
+namespace Calculator
+{
+    class Calc
+    {
+
+        static void Main()
+        {
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter your choice. 1-Add, 2-Sub, 3-Mul, 4-Division, 5-Exit");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int firstNumber = 0, secondNumber = 0;
+                if (userInput != 5)
+                {
+                    Console.WriteLine("Please enter your first number");
+                    firstNumber = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter your second number");
+                    secondNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                
+
+                string result = String.Empty;
+
+                if (userInput == 1)
+                {
+                    result = "Sum of " + firstNumber + " + " + secondNumber + " = " + CalculatorDAL.Sum(firstNumber, secondNumber);
+                }
+                else if (userInput == 2)
+                {
+                    result = "Subtract of " + firstNumber + " - " + secondNumber + " = " + CalculatorDAL.Subtraction(firstNumber, secondNumber);
+                }
+                else if (userInput == 3)
+                {
+                    result = "Multiply of " + firstNumber + " * " + secondNumber + " = " + CalculatorDAL.Multiplication(firstNumber, secondNumber);
+                }
+                else if (userInput == 4)
+                {
+                    result = "Division of " + firstNumber + " / " + secondNumber + " = " + CalculatorDAL.Division(firstNumber, secondNumber);
+                }
+                else if (userInput == 5)
+                {
+                    isExit = true;
+                    Console.WriteLine("Thank you for visiting");
+                }
+                else 
+                {
+                    Console.WriteLine("Wrong input.Try again");
+                }
+                Console.WriteLine(result);
+            }                       
+        }
+    }
+}
